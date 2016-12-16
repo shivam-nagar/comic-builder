72c22837ce3cc56a6c91cdf7ac95b1edc756754b
sreeraj.a <sreeraj.a@inmobi.com>
2016-12-17 00:09:33 +0530

    post file

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
+from django import forms
+import tempfile
+
+from django.http import JsonResponse
+
+from wordpress_xmlrpc import Client, WordPressPost
+from wordpress_xmlrpc.compat import xmlrpc_client
+from wordpress_xmlrpc.methods import media, posts
+
+class UploadFileForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    file = forms.FileField()
 
 # Create your views here.
 def index(request):
@@ -9,12 +20,30 @@ def index(request):
     return render(request, 'index.html')
 
 
-def db(request):
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            attachment_id = handle_uploaded_file(request.FILES['file'])
+            post = WordPressPost()
+            post.title = form.title
+            post.content = ''
+            post.post_status = 'publish'
+            post.thumbnail = attachment_id
+            post.id = client.call(posts.NewPost(post))
 
-    greeting = Greeting()
-    greeting.save()
+            return JsonResponse({'success':True, 'data':{'post_id':post.id}})
+    else:
+            return JsonResponse({'success':False, 'data':{}})
 
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})
-
+def handle_uploaded_file(f):
+    client = Client('https://3panelstories.wordpress.com/xmlrpc.php', '3panelstories', 'Tr0janh0rse')
+    data = {
+            'name': 'picture.jpg',
+            'type': 'image/jpeg',  # mimetype
+            }
+    for chunk in f.chunks():
+        data['bits'] = xmlrpc_client.Binary(img.read())
+    response = client.call(media.UploadFile(data))
+    attachment_id = response['id']
+    return attachment_id
